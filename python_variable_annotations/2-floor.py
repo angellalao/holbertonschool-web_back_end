94ea2b946e9d4900a49f8d914ca70f9b0bd99bc7
angellalao <6369@holbertonstudents.com>
2023-08-27 23:34:40 -0700

    floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""type-annotated function 'floor'"""
+import math
+
+
+def floor(n: float) -> int:
+    """converts a float to the floor of the float"""
+    result = math.floor(n)
+    return result

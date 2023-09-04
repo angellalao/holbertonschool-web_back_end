3d78ebcd60e518877370a6676479f956408167b1
angellalao <6369@holbertonstudents.com>
2023-09-04 00:57:38 -0700

    refactored

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -12,16 +12,12 @@ if __name__ == "__main__":
     docs = collection.count_documents({})
     print(f"{docs} logs")
     print("Methods:")
-    get = collection.count_documents({"method": "GET"})
-    print(f"\tmethod GET: {get}")
-    post = collection.count_documents({"method": "POST"})
-    print(f"\tmethod POST: {post}")
-    put = collection.count_documents({"method": "PUT"})
-    print(f"\tmethod PUT: {put}")
-    patch = collection.count_documents({"method": "PATCH"})
-    print(f"\tmethod PATCH: {patch}")
-    delete = collection.count_documents({"method": "DELETE"})
-    print(f"\tmethod DELETE: {delete}")
-    method_path = collection.count_documents(
-        {"method": "GET", "path": "/status"})
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    for method in methods:
+        lower_str = method.lower()
+        lower_str = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {lower_str}")
+
+        method_path = collection.count_documents(
+            {"method": "GET", "path": "/status"})
     print(f"{method_path} status check")

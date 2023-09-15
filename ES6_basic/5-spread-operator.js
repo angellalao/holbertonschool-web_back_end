1b47f99f9e7aaf1636d08896391651d9d2323cde
Angella Lao <6369@holbertonstudents.com>
2023-09-15 10:27:12 +0000

    concatenate arrays

--- ES6_basic/5-spread-operator.js
+++ ES6_basic/5-spread-operator.js
@@ -1,5 +1,5 @@
 export default function concatArrays(array1, array2, string) {
   const array3 = [...string];
-  array1 = [...array1, ...array2, ...array3];
-  return array1;
+  const array = [...array1, ...array2, ...array3];
+  return array;
 }

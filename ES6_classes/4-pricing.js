a1806c64f6d6dbf9f3fcfe3ed827896ab5d2db69
Angella Lao <6369@holbertonstudents.com>
2023-09-17 06:06:23 +0000

    implement method and static method

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,44 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number.');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number.');
+    }
+    return amount * conversionRate;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number.');
+    }
+    this._amount = value;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Currency must be a currency.');
+    }
+    this._currency = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} ${(this.currency.code)}`;
+  }
+}

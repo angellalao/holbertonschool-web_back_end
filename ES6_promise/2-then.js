92d7071e412658ed1e304f85c3085f32547b4bc0
Angella Lao <6369@holbertonstudents.com>
2023-10-03 21:31:06 +1100

    use finally callback

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,10 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return {
-      body: 'success',
-      status: 200,
-    };
-  })
-    .catch(() => new Error());
+  return promise
+    .then(() => ({ body: 'success', status: 200 }))
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
 }

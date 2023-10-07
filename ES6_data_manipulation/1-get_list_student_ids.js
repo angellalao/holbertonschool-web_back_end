b748fe964f50866b5a2e30adbff69ebf9a7e4677
Angella Lao <6369@holbertonstudents.com>
2023-10-07 22:11:47 +1100

    map method

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+export default function getListStudentIds(arrayObjs) {
+  if (!Array.isArray(arrayObjs)) {
+    return [];
+  }
+  const newArray = arrayObjs.map((x) => x.id );
+  return newArray;
+}

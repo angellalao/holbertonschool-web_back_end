2563ca25238763bc668038f2026554ecf37a403f
Angella Lao <6369@holbertonstudents.com>
2023-10-07 16:58:34 +1100

    edit

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,18 +2,18 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  const [userResult, fileResult] = await Promise.all([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName)]);
-
-  const userPromise = {
-    status: userResult.status === 'fulfilled' ? 'fulfilled' : 'rejected',
-    value: userResult.status === 'fulfilled' ? userResult.value : userResult.reason,
-  };
-
-  const filePromise = {
-    status: fileResult.status === 'fulfilled' ? 'fulfilled' : 'rejected',
-    value: fileResult.status === 'fulfilled' ? fileResult.value : fileResult.reason,
-  };
-  return Promise.resolve([userPromise, filePromise]);
+  try {
+    const [userData, fileData] = await Promise.all([
+      signUpUser(firstName, lastName),
+      uploadPhoto(fileName)]);
+    return [
+      { status: 'fulfilled', value: userData },
+      { status: 'fulfilled', value: fileData },
+    ];
+  } catch (error) {
+    return [
+      { status: 'rejected', value: error.toString() },
+      { status: 'rejected', value: error.toString() },
+    ];
+  }
 }

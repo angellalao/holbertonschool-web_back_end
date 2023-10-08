ba2438eb23214a6c992440427168bbcd9445f695
Angella Lao <6369@holbertonstudents.com>
2023-10-08 16:50:45 +1100

    clean set

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') {
+    return '';
+  }
+  const list = [];
+  for (const word of set) {
+    if (word.startsWith(startString)) {
+      const newString = word.replace(startString, '');
+      list.push(newString);
+    }
+  }
+  return list.join('-');
+}

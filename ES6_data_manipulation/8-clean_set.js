dbb32dd1d5b25409a9fb008c344250faa813d4b7
Angella Lao <6369@holbertonstudents.com>
2023-10-08 17:36:40 +1100

    edit

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
+  if (startString === '' || startString === null || typeof (startString) !== 'string') {
     return '';
   }
   const list = [];
